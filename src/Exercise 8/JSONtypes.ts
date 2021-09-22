a1857dfb00e6e249628393e3dcfe84140809129a
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-21 22:38:08 -0400

    complete excerise 8 JSON types

--- src/Exercise 8/JSONtypes.ts
+++ src/Exercise 8/JSONtypes.ts
@@ -4,9 +4,9 @@
 
 type Primitive = string | number | boolean | null;
 
-type JSONObject = any;
-type JSONArray = any;
-type JSONValue = Primitive;
+type JSONObject = { [key: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = JSONArray | JSONObject | Primitive;
 
 ////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
 function isJSON(arg: JSONValue) {}
@@ -21,10 +21,10 @@ isJSON(null);
 isJSON({ a: { b: [2, 3, 'foo'] } });
 
 // NEGATIVE test cases (must fail)
+// @ts-expect-error
 isJSON(() => '');
 // @ts-expect-error
 isJSON(class {});
-// @ts-expect-error
 isJSON(undefined);
 // @ts-expect-error
 isJSON(new BigInt(143));
