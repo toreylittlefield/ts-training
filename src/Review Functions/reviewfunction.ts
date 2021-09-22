26273255734aaef3aee4ed5281b4133bd0d0951f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-22 06:15:45 -0400

    finish review function by fixing all the type errors

--- src/Review Functions/reviewfunction.ts
+++ src/Review Functions/reviewfunction.ts
@@ -1,20 +1,20 @@
-function useMagnifyingGlass(): string {
+function useMagnifyingGlass(): void {
   console.log('I will use my magnifying glass.');
 }
 
-function determineCulprit() {
+function determineCulprit(): number {
   return Math.floor(Math.random() * 2 + 1);
 }
 
-function doSleuthing(numberOfClues: number, clue1: string, clue2: string, suspect1: string, suspect2: number): void {
+function doSleuthing(numberOfClues: number, clue1: string, clue2: string, suspect1: string, suspect2: string): string {
   console.log('I am a famous detective and I will solve the crime.');
-  let unnecessaryVariable = 'Why is this here?';
-  unnecessaryVariable = useMagnifyingGlass();
+  //   let unnecessaryVariable = 'Why is this here?';
+  useMagnifyingGlass();
 
   console.log('Now I consider the first clue: ', clue1);
   console.log('Now I consider the second clue: ', clue1);
 
-  let culpritNumber: string = determineCulprit();
+  let culpritNumber = determineCulprit();
   console.log('Now, I will return to you the culprit. There but for the grace of God go we.');
   if (culpritNumber == 1) {
     return suspect1;
@@ -26,10 +26,10 @@ function doSleuthing(numberOfClues: number, clue1: string, clue2: string, suspec
   return "I couldn't figure out who drank the priceless milk. :( :(";
 }
 
-let answer = 3;
+let answer: string;
 
 answer = doSleuthing(
-  '2',
+  2,
   'The parrot heard everything!',
   'All the doors and windows were shut from the INSIDE.',
   'Burglar Bob',
