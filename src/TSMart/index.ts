23c4d196140758c386943676fd2d7660916b0a7d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-21 23:30:52 -0400

    add shipping and conditionals

--- src/TSMart/index.ts
+++ src/TSMart/index.ts
@@ -7,11 +7,18 @@ let total: number;
 
 let shippingAddress: string = '555 Some Street, Some City, Some State, Some Country';
 
-const productName: string = 'fanny pack';
+const productName: string = 'shirt';
 const product = products.find((product) => product.name === productName);
 
 console.log(product);
 
-if (product.preOrder === true) {
+if (product?.preOrder === true) {
   console.log('We will send you a message when your product is on its way.');
+}
+
+if (Number(product?.price) < 25) {
+  shipping = 5;
+} else {
+  shipping = 0;
+  console.log(`Free Shipping will be provided for this order since it is over $25`);
 }
