cab0077c777e8c026d010f7414b373fc0c753852
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-22 21:35:07 -0400

    start example generic functions

---
+++ src/TS Types/genericfunctions.ts
@@ -0,0 +1,10 @@
+function getFilledArray<T>(value: T, n: number): T[] {
+  return Array(n).fill(value);
+}
+
+let stringArray: string[];
+let numberArray: number[];
+let personArray: { name: string; age: number }[];
+let coordinateArray: [number, number][];
+
+// Write your code below:
