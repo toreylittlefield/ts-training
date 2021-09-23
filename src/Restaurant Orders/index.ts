3f740a0e5199046aa1dec37b7c621cd66ad2e70d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-23 09:20:39 -0400

    add getMaxPrice

--- src/Restaurant Orders/index.ts
+++ src/Restaurant Orders/index.ts
@@ -2,7 +2,19 @@ import { restaurants, Restaurant } from './restaurants';
 import { orders, Order, PriceBracket } from './orders';
 
 /// Add your getMaxPrice() function below:
-
+/// Add your getMaxPrice() function below:
+const getMaxPrice = (price: PriceBracket): number => {
+  switch (price) {
+    case PriceBracket.Low:
+      return 10.0;
+    case PriceBracket.Medium:
+      return 20.0;
+    case PriceBracket.High:
+      return 30.0;
+    default:
+      return 10.0;
+  }
+};
 /// Add your getOrders() function below:
 
 /// Add your printOrders() function below:
