40ac14903f7f79f63e0cf7a1851d55909bf203d3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-24 07:24:54 -0400

    deep types example interface classes

---
+++ src/Interface and Types/deeptypesexamples.ts
@@ -0,0 +1,31 @@
+interface Directory {
+  addFile: (name: string) => void;
+  // Define a config type member here
+  config: {
+    default: {
+      encoding: string;
+      permissions: string;
+    };
+  };
+}
+
+class DesktopDirectory2 implements Directory {
+  config = {
+    default: {
+      encoding: 'utf-8',
+      permissions: 'drw-rw-rw-',
+    },
+  };
+
+  addFile(name: string) {
+    console.log(`Adding file: ${name}`);
+  }
+
+  showPreview(name: string) {
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
+
+const Desktop2 = new DesktopDirectory2();
+
+console.log(Desktop2.config);
