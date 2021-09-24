c302034c335e31105af4cd73f8a0a75a730d0584
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-24 01:14:11 -0400

    type guarding with custom types (using "in" operator)

--- src/Type Narrowing/basictypenarrowingexamples.ts
+++ src/Type Narrowing/basictypenarrowingexamples.ts
@@ -10,3 +10,33 @@ function formatStatistic(stat: string | number) {
 
 console.log(formatStatistic('Win'));
 console.log(formatStatistic(0.364));
+
+// custom type guarding example
+type Cat = {
+  name: string;
+  run: () => string;
+};
+
+type Fish = {
+  name: string;
+  swim: () => string;
+};
+
+const siameseCat = {
+  name: 'Proxie',
+  run: () => 'pitter pat',
+};
+
+const bettaFish = {
+  name: 'Neptune',
+  swim: () => 'bubble blub',
+};
+
+function move(pet: Cat | Fish) {
+  if ('run' in pet) {
+    return pet.run();
+  }
+  return pet.swim();
+}
+
+console.log(move(siameseCat));
