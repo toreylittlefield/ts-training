eaf52b7ee3c4c0a1d3a20c38695538c6c2dcde2e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-23 20:07:14 -0400

    basic union string | number function args

--- src/Unions/basicexample.ts
+++ src/Unions/basicexample.ts
@@ -4,3 +4,10 @@ function printNumsAndStrings(statement) {
 }
 
 printNumsAndStrings('hello!');
+
+// after string or number type union
+function printNumsAndStringsTS(statement: string | number) {
+  console.log(`ℹ️ LOG:: ${statement}`);
+}
+
+printNumsAndStringsTS('hello!');
