2ca86c86039d6afd69d31e25ff4e90b25d0359d9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-23 20:36:49 -0400

    an array union example

--- src/Unions/inferredunionreturntypes.ts
+++ src/Unions/inferredunionreturntypes.ts
@@ -15,3 +15,20 @@ function createUser() {
 
 // the return of createUser fn can be returned as a string or User type
 const userData: string | User = createUser();
+
+// an array example
+function formatListings(listings: (string | number)[]) {
+  return listings.map((listing) => {
+    if (typeof listing === 'string') {
+      return listing.toUpperCase();
+    }
+
+    if (typeof listing === 'number') {
+      return `$${listing.toLocaleString()}`;
+    }
+  });
+}
+
+const result = formatListings(['123 Main St', 226800, '580 Broadway Apt 4a', 337900]);
+
+console.log(result);
