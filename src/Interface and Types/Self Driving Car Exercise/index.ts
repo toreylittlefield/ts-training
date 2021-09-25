eef201d8b4244e7eb116059c97a748a3d73c056e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-25 01:18:21 -0400

    half through exercise

--- src/Interface and Types/Self Driving Car Exercise/index.ts
+++ src/Interface and Types/Self Driving Car Exercise/index.ts
@@ -1,1 +1,34 @@
 import { getObstacleEvents } from './computer-vision';
+
+interface Events {
+  [eventName: string]: boolean;
+}
+
+interface AutonomousCar {
+  isRunning: boolean;
+  respond: (events: Events) => void;
+}
+
+interface AutonomousCarProps {
+  isRunning?: boolean;
+}
+
+class Car implements AutonomousCar {
+  isRunning;
+  constructor(props: AutonomousCarProps) {
+    this.isRunning = props.isRunning;
+  }
+
+  respond(events: Events) {
+    if (this.isRunning === false) {
+      console.log('The car is off.');
+      return;
+    }
+  }
+}
+
+// const autonomousCar = new Car({ isRunning: true });
+const autonomousCar = new Car({ isRunning: true }).respond(getObstacleEvents());
+console.log(autonomousCar);
+
+// console.log(autonomousCar.isRunning);
