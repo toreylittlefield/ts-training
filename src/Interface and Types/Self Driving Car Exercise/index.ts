d472a3d26d64fa12832e77e8b10c9bfdb81d4b43
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-25 01:48:13 -0400

    finish Self Driving Car Example

--- src/Interface and Types/Self Driving Car Exercise/index.ts
+++ src/Interface and Types/Self Driving Car Exercise/index.ts
@@ -11,12 +11,23 @@ interface AutonomousCar {
 
 interface AutonomousCarProps {
   isRunning?: boolean;
+  steeringControl: Steering;
+}
+
+interface Control {
+  execute: (command: string) => void;
+}
+
+interface Steering extends Control {
+  turn: (direction: string) => void;
 }
 
 class Car implements AutonomousCar {
   isRunning;
+  steeringControl;
   constructor(props: AutonomousCarProps) {
     this.isRunning = props.isRunning;
+    this.steeringControl = props.steeringControl;
   }
 
   respond(events: Events) {
@@ -24,11 +35,32 @@ class Car implements AutonomousCar {
       console.log('The car is off.');
       return;
     }
+    Object.keys(events).forEach((eventKey) => {
+      if (!eventKey) return;
+      switch (eventKey) {
+        case 'ObstacleLeft':
+          this.steeringControl.turn('right');
+          break;
+        case 'ObstacleRight':
+          this.steeringControl.turn('left');
+          break;
+      }
+    });
+  }
+}
+
+class SteeringControl implements Steering {
+  execute(command: string) {
+    console.log(`Executing: ${command}`);
+  }
+  turn(direction: string) {
+    this.execute(`turn ${direction}`);
   }
 }
 
 // const autonomousCar = new Car({ isRunning: true });
-const autonomousCar = new Car({ isRunning: true }).respond(getObstacleEvents());
-console.log(autonomousCar);
+const steering = new SteeringControl();
 
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
+autonomousCar.respond(getObstacleEvents());
 // console.log(autonomousCar.isRunning);
